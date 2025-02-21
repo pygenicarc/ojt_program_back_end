9da2f4f4d72d64cd298110b25a9c6c193c2e8957
pygenicarc <pygenicarc@gmail.com>
2025-02-21 19:28:46 +0530

    app file set up

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+
+todo = Flask(__name__)
+
+
+@todo.route('/')
+def index():
+    return "Hello Home page"
+
+
+@todo.route('/about')
+def about():
+    return "Hello about page"
+
+
+@todo.route('/contact')
+def contact():
+    return "Hello contact page"
+
+
+@todo.route('/python-course')
+def python_course():
+    return "Hello python course page"
+
+
+# I need host, port
+if __name__ == '__main__':
+    todo.run(
+        host='127.0.0.1',
+        port=5005,
+        debug=True
+    )
